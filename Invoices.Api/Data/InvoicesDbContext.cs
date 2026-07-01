981d5378411616ecbfbe35b0728960c90a094df5
Jane Kotovich <jenyakotovich@gmail.com>
2026-07-01 09:46:24 +0200

    Updated DbContext constructor

--- Invoices.Api/Data/InvoicesDbContext.cs
+++ Invoices.Api/Data/InvoicesDbContext.cs
@@ -4,9 +4,12 @@ using Microsoft.EntityFrameworkCore;
 using Invoices.Api.Model;
 
 
-public class InvoicesDbContext(DbContextOptions<InvoicesDbContext> options) : DbContext(options)
+public class InvoicesDbContext : DbContext
 {
+    public InvoicesDbContext(DbContextOptions<InvoicesDbContext> options)
+        : base(options) { }
 
     public DbSet<Invoice> Invoices => Set<Invoice>();
-
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Client> Clients => Set<Client>();
 }
